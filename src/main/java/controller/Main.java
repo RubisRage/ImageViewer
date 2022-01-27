bf961c387409d958bc2df47d7f5eb8398b4e6d3f
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2022-01-27 00:24:44 +0000

    Added directory selector

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -3,14 +3,23 @@ package controller;
 import model.Image;
 import view.persistence.FileImageLoader;
 
+import javax.swing.*;
 import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("/home/rubisrage/Mis Cosas/Imagenes/Wallpaper/");
-        FileImageLoader imageLoader = new FileImageLoader(file);
-        Image image = imageLoader.load();
-        MainFrame mainFrame = new MainFrame(imageLoader);
-        mainFrame.getImageDisplay().show(image);
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        JOptionPane.showMessageDialog(null, "Please select a folder to load images from",
+                "Welcome!", JOptionPane.INFORMATION_MESSAGE);
+
+        if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            FileImageLoader imageLoader = new FileImageLoader(file);
+            Image image = imageLoader.load();
+            MainFrame mainFrame = new MainFrame(imageLoader);
+            mainFrame.getImageDisplay().show(image);
+        }
     }
 }
